6f3418b1527c22833ca3d551c3be6777a0b00780
simonbf15 <barralesimon@gmail.com>
2024-06-01 22:09:38 -0300

    subo cambios tengo que mejorar la url del endpoint para funcion search

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,12 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import { Header } from './componentes/encabezado';
+import { useEffect, useState } from 'react';
+import { queryByTestId } from '@testing-library/react';
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
-}
 
-export default App;
+  const [query, setQuery] = useState([])
+  const [cursos, setCursos] = useState('')
+  const [buscar, setBuscar] = useState('')
+
+  useEffect(() => { "/cursos/:id"
+    fetch('http://localhost:8080/cursos/:id?query=${search}')
+      .then(response => response.json())
+      .then(data => setCursos(data.results))
+      .catch(error => console.error('error al buscar el curso solicitado', error))
+
+  }, [])
+
+
+  const cargarDatos = (e) =>{
+console.log('estas buscando: ' + {query})
+setQuery(e.target.value)
+  }
+
+
+const Buscar = (e) => {
+    setBuscar(query)
+  }
+
+    return (
+      <div className="App">
+
+        <div className='buscador'>
+          <input
+            type='text'
+            placeholder='Introducir curso deseado...'
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button onClick={() => Buscar}>Buscar</button>
+        </div>
+
+
+      </div>
+    );
+  }
+
+  export default App;
