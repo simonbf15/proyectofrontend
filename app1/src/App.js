7717503c2f367a3bc8b2ba1dd5cd58ef6687fcf4
simonbf15 <barralesimon@gmail.com>
2024-06-01 22:24:32 -0300

    idem

--- app1/src/App.js
+++ app1/src/App.js
@@ -8,40 +8,48 @@ function App() {
   const [cursos, setCursos] = useState('')
   const [buscar, setBuscar] = useState('')
 
-  useEffect(() => { "/cursos/:id"
-    fetch('http://localhost:8080/cursos/:id?query=${search}')
-      .then(response => response.json())
-      .then(data => setCursos(data.results))
-      .catch(error => console.error('error al buscar el curso solicitado', error))
+  /* useEffect(() => { 
+     fetch('http://localhost:8080/cursos/:id?query=${search}')
+       .then(response => response.json())
+       .then(data => setCursos(data.results))
+       .catch(error => console.error('error al buscar el curso solicitado', error))
+ 
+   }, [])
+   
+   aca tengo que cargar bien el endpoint 
+   */
 
-  }, [])
 
+  const cargarDatos = (e) => {
+    setQuery(e.target.value)
+    console.log('estas buscando: ' + query)
 
-  const cargarDatos = (e) =>{
-console.log('estas buscando: ' + {query})
-setQuery(e.target.value)
   }
 
 
-const Buscar = (e) => {
+  const Buscar = (e) => {
     setBuscar(query)
+    console.log('se cargó la busqueda: ' + buscar)
   }
 
-    return (
-      <div className="App">
+  return (
+    <div className="App">
 
-        <div className='buscador'>
-          <input
-            type='text'
-            placeholder='Introducir curso deseado...'
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <button onClick={() => Buscar}>Buscar</button>
-        </div>
+      <div className='buscador'>
+        <input
+          type='text'
+          placeholder='Introducir curso deseado...'
+          onChange={cargarDatos}
+        />
+        <button onClick={Buscar}>Buscar</button>
+      </div>
 
+      <div className='muestraCursos'>
 
       </div>
-    );
-  }
 
-  export default App;
+    </div>
+  );
+}
+
+export default App;
